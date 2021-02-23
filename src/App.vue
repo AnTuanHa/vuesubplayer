2299e14be459e7c7187f0f54a23874b36013f1d6
An Ha <zero579911@gmail.com>
2021-02-22 22:27:55 -0500

    feat: add ability to drag and drop video
    * Use HTML5 <video> tag to play videos

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app" @drop.prevent.stop="onDropEvent" @dragover.prevent.stop>
+    <main class="app-main">
+      <VideoPlayer :video-url="videoUrl" :subtitles-url="subtitlesUrl" />
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, ref } from "vue";
+
+import VideoPlayer from "./components/VideoPlayer.vue";
+
+const isSubtitleFile = function(file: File) {
+  return /\.(srt|ass)$/.test(file.name);
+};
 
 export default defineComponent({
   name: "App",
+
   components: {
-    HelloWorld
+    VideoPlayer
+  },
+
+  setup() {
+    const subtitlesUrl = ref("");
+    const videoUrl = ref("");
+
+    const loadSubtitleFile = (file: File) => {
+      // todo: add load subtitles
+      return;
+    };
+
+    const loadVideoFile = (file: File) => {
+      if (videoUrl) {
+        URL.revokeObjectURL(videoUrl.value);
+      }
+      videoUrl.value = URL.createObjectURL(file);
+    };
+
+    return {
+      subtitlesUrl,
+      videoUrl,
+      onDropEvent: (e: DragEvent) => {
+        if (!e.dataTransfer) {
+          return;
+        }
+        [...e.dataTransfer.files].forEach(file => {
+          isSubtitleFile(file) ? loadSubtitleFile(file) : loadVideoFile(file);
+        });
+      }
+    };
   }
 });
 </script>
 
 <style>
+html,
+body {
+  margin: 0;
+  height: 100vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  display: flex;
+  flex-flow: row nowrap;
+  position: relative;
+  background-color: black;
+}
+
+.app-main {
+  flex: 1 80vw;
+  position: relative;
 }
 </style>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="video-player">
+    <video class="video-element" :src="videoUrl" crossorigin controls>
+      <track kind="subtitles" :src="subtitlesUrl" default />
+      Your browser does not support the video tag
+    </video>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VideoPlayer",
+
+  props: {
+    videoUrl: {
+      type: String,
+      default: ""
+    },
+    subtitlesUrl: {
+      type: String,
+      default: ""
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.video-player {
+  height: 100vh;
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.video-element {
+  width: 100%;
+}
+
+/* Hide browser's built-in subtitles */
+.video-player ::cue {
+  visibility: hidden;
+  color: transparent;
+}
+
+.video-player ::-webkit-media-controls-toggle-closed-captions-button,
+.video-player ::-webkit-media-text-track-container {
+  display: none;
+}
+</style>
