baf8b30bfabe7effc57b38e378f69e1d4f0e16fb
An Ha <zero579911@gmail.com>
2021-02-22 22:19:31 -0500

    lint: use VueJS recommended lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
@@ -10,22 +12,33 @@ module.exports = {
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint"
   ],
+
   parserOptions: {
     ecmaVersion: 2020
   },
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': 'off',
+    'no-debugger': 'off'
   },
+
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         mocha: true
       }
     }
+  ],
+
+  'extends': [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
   ]
 };
