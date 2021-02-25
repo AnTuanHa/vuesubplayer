54a3c2ddddc13e56c2bcab2c4ff763e11adbf819
An Ha <zero579911@gmail.com>
2021-02-25 00:37:10 -0500

    remove unused variable

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -43,7 +43,7 @@ export default defineComponent({
 
     watch(
       () => store.state.currentTime,
-      (newTime, oldTime) => {
+      newTime => {
         if (!videoElement.value) {
           return;
         }
